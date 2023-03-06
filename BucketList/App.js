d5c1b99a235bb9a56db7749cbbecc72512259f0a
Fredrick Apollo <57845077+apollofredrick@users.noreply.github.com>
2023-03-06 13:36:47 +0300

    user interface design
    app ui design and passing props

---
+++ BucketList/App.js
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './Task';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/*today's tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/*this is where the tasks will go */}
+          <Task text={"Task 1"}/>
+          <Task text={"Task 2"}/>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e8eaed',
+  },
+  tasksWrapper:{
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle:{
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items:{
+    marginTop: 30,
+  }
+});
---
+++ BucketList/Task.js
@@ -0,0 +1,53 @@
+
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+
+const Task = (props) => {
+  return (
+    <View style={styles.item}>
+        <View style={styles.itemLeft}>
+            <View style={styles.square}></View>
+            <Text style={styles.itemText}>{props.text}</Text>
+        </View>
+        <View style={styles.circular}></View>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    item:{
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    itemLeft:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+    square:{
+        backgroundColor: '#55bcf6',
+        height: 24,
+        width: 24,
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
+    },
+    itemText:{
+        maxWidth: '80%'
+    },
+    circular:{
+        height: 12,
+        width: 12,
+        borderColor: '#55bcf6',
+        borderRadius: 5,
+        borderWidth: 3,
+    }
+})
+
+export default Task
